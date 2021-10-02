90f3a4269841047aebcdf5d76b56bc11c2269d49
michalo482 <77701451+michalo482@users.noreply.github.com>
2021-10-02 17:49:40 +0200

    korekty

---
+++ emerytura.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace pierwsze_repo_uczelnia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj imie:");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Hello " + userName);
+            Console.WriteLine("Podaj swoj wiek:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            if(age > 60)
+            {
+               Console.WriteLine("jestes na emeryturze " + userName); 
+            } 
+            else
+            {
+                int left = 60 - age;
+                Console.WriteLine("do emerytury zostalo ci: " + left);
+            }
+        }
+    }
+}
